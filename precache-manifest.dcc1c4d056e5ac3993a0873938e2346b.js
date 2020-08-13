edfb3b48ea5ce1f0c77d6ab5c774dfb36136a370
toxa <Saint2202*>
2020-08-13 21:14:55 +0300

    Updates

---
+++ precache-manifest.dcc1c4d056e5ac3993a0873938e2346b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da7f07e0266b2f45d33e78f4be2a4ff",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "78c7f849462c00a0a520",
+    "url": "/robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "109591d57d43ccdb06ec",
+    "url": "/robofriend/static/css/main.f680504e.chunk.css"
+  },
+  {
+    "revision": "78c7f849462c00a0a520",
+    "url": "/robofriend/static/js/2.be3d66de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend/static/js/2.be3d66de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "109591d57d43ccdb06ec",
+    "url": "/robofriend/static/js/main.0eeb7cf3.chunk.js"
+  },
+  {
+    "revision": "b9549992ed9aa0bec102",
+    "url": "/robofriend/static/js/runtime-main.b099a5de.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
